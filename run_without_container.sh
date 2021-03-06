9365f1da1e18f90f9654eac5320aa38fc9f343b8
raelix <raelix@hotmail.it>
2021-03-06 20:43:28 +0100

    tuned settings

--- run_without_container.sh
+++ run_without_container.sh
@@ -5,10 +5,10 @@ export REQUESTS_TIMEOUT=10
 export WAIT_BEFORE_LOGIN=1800
 export STANDALONE_PROXY=true
 export PROXY_TYPE=tor
-export AMAZON_EMAIL=raelix@hotmail.it
+export AMAZON_EMAIL=
 export AMAZON_PASSWORD=
 export PORTAL=https://www.amazon.it
-export IS_TEST=false
+export IS_TEST=False
 export USE_TOR=True
 export IS_CONTAINERIZED=false
 cd src && python3 main.py
